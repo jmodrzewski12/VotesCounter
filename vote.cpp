823d93a4885bacd705acb9c0f056206439997b86
modrzew <modrzewoss@gmail.com>
2021-09-05 10:36:50 +0200

    Add project files.

---
+++ vote.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	int N = 0;  // number of candidates
+	int M = 0;  // number of votes
+
+	cin >> N >> M;
+
+	cout << "Hello World!\n";
+
+	return 0;
+}
