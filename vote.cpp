a7c9f0612f88f60b1fe8008a3bd6f84e6fa8aca3
modrzew <modrzewoss@gmail.com>
2021-09-05 11:45:34 +0200

    Added reading votes from text file

--- vote.cpp
+++ vote.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 
-
 int main()
 {
+	const string testFilename = "test1.txt";
+
+	ifstream ifs(testFilename, ifstream::in);
+	if (!ifs.good())  // checks if filename is correct
+	{
+		cout << "Cannot find input file.\n";
+		return 1;
+	}
+
+	streambuf* sb = ifs.rdbuf();  // gets pointer to the stream buffer
+	cin.rdbuf(sb);  // assing streambuf to cin
+
 	int N = 0;  // number of candidates
 	int M = 0;  // number of votes
+	cin >> N >> M;  // gets N and M from streambuf
 
-	cin >> N >> M;
+	int v;
+	vector<int> votes = {};
 
-	cout << "Hello World!\n";
+	while (cin >> v)
+	{
+		cout << v;
+		votes.push_back(v);
+	}
+
+
+	
+	
+
+	//char c = ifs.get();
+	//cout << getNumberOfCandidates(ifs);
+	//cout << c;
+
+	ifs.close();
+	
 
 	return 0;
 }
